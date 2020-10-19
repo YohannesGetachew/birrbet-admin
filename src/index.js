b6dd2995bff1dc84d5f0315245ab3665a1a1e643
j o <moneymakr37@gmail.com>
2020-10-19 19:44:20 +0300

    app to App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
-import App from "./app";
+import App from "./App";
 import customApolloClient from "./apollo";
 import { AuthContextProvider } from "./contexts/auth";
 import "vanilla-antd-message/dist/style.css";
